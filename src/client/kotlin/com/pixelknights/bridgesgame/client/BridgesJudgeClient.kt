26f5be54b576d0357df76f83c7a8f513320f096a
PixelKnights <pixelknights5@gmail.com>
2024-12-14 18:06:38 -0600

    Setup logging framework

--- src/client/kotlin/com/pixelknights/bridgesgame/client/BridgesJudgeClient.kt
+++ src/client/kotlin/com/pixelknights/bridgesgame/client/BridgesJudgeClient.kt
@@ -1,9 +1,17 @@
 package com.pixelknights.bridgesgame.client
 
 import net.fabricmc.api.ClientModInitializer
+import org.apache.logging.log4j.LogManager
 
 class BridgesJudgeClient : ClientModInitializer {
 
     override fun onInitializeClient() {
+        logger.info("Initializing BridgesJudge Mod")
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    companion object {
+        const val MOD_ID = "bridges-judge"
+        val logger = LogManager.getLogger(MOD_ID)
     }
 }
