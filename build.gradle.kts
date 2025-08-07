779c8468b050114484355416e27d790529a9f80d
PixelKnights <pixelknights5@gmail.com>
2025-08-06 21:32:26 -0500

    Update to run in Minecraft 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "1.5.31"
-    id("fabric-loom") version "1.9-SNAPSHOT"
+    id("fabric-loom") version "1.11-SNAPSHOT"
     id("maven-publish")
 }
 
