81015a9b8677e506d1546a0b2a0fc7fb87697401
PixelKnights_ <pixelknights5@gmail.com>
2026-07-11 20:15:01 -0500

    Towers are now auto claimed when all floors are captured (#26)
    Co-authored-by: PixelKnights <192544013+PixelKnights5@users.noreply.github.com>

--- src/client/kotlin/com/pixelknights/bridgesgame/client/game/entity/Tower.kt
+++ src/client/kotlin/com/pixelknights/bridgesgame/client/game/entity/Tower.kt
@@ -2,8 +2,6 @@ package com.pixelknights.bridgesgame.client.game.entity
 
 import com.google.common.base.Objects
 import com.pixelknights.bridgesgame.client.config.ModConfig
-import com.pixelknights.bridgesgame.client.util.getTeamColorForBlock
-import net.minecraft.client.world.ClientWorld
 import net.minecraft.util.math.BlockPos
 
 class Tower(
@@ -30,30 +28,18 @@ class Tower(
         return BlockPos(worldX, centerTowerPos.y, worldZ)
     }
 
-    /**
-     * Get the team that placed the tower claim block, but don't confirm that the claim is valid.
-     * To check if the claim is valid, use [setCapturingTeam] then check the [capturingTeam] property.
-     */
-    fun getAttemptedClaimingTeam(world: ClientWorld, config: ModConfig): GameColor? {
-        val topFloor = floors[numFloors - 1]
-        val ceilingBlockOffset = config.towerConfig.blocksBetweenFloors / 2
-        val ceilingBlock = topFloor.worldCenter.up(ceilingBlockOffset)
-        return getTeamColorForBlock(world.getBlockState(ceilingBlock)?.block)
-    }
-
-    fun setCapturingTeam(world: ClientWorld, config: ModConfig) {
+    fun setCapturingTeam() {
         val firstColor = floors.firstOrNull()?.captureColor
         if (firstColor == null) {
             return
         }
 
         val areAllFloorsCaptured = floors.all { it.owner == firstColor && it.isOwnerValidated }
-        val attemptedClaimingTeam = getAttemptedClaimingTeam(world, config)
-        val doesCeilingBlockMatch = attemptedClaimingTeam == firstColor
 
-        capturingTeam = when {
-            areAllFloorsCaptured && doesCeilingBlockMatch -> firstColor
-            else -> null
+        capturingTeam = if (areAllFloorsCaptured) {
+            firstColor
+        } else {
+            null
         }
     }
 
